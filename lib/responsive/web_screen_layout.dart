95f787d21e1074f23fccdd125b9b8d1dbfa44707
vineet22vishwakarma@gmail.com <vineet22vishwakarma@gmail.com>
2024-01-01 00:05:39 +0530

    navigation for web

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:search_engine_google_clone/colors.dart';
 
 class WebScreenLayout extends StatelessWidget {
@@ -31,6 +32,28 @@ class WebScreenLayout extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            // ignore: deprecated_member_use
+            icon: SvgPicture.asset('assets/images/more-apps.svg',color: primaryColor,),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:10.0).copyWith(right: 10),
+            child: MaterialButton(
+              color: const Color(0xff1A73EB),
+              onPressed: () {},
+              child: const Text(
+                'Sign In',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
         ],
       ),
     );
