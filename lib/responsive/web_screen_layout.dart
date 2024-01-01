0a27f8524adef3e0f46ff65432d66ed3a80a68c0
vineet22vishwakarma@gmail.com <vineet22vishwakarma@gmail.com>
2024-01-01 17:43:43 +0530

    searchbar for web screen

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:search_engine_google_clone/colors.dart';
+import 'package:search_engine_google_clone/widgets/search.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: backgroundColor,
@@ -38,13 +40,17 @@ class WebScreenLayout extends StatelessWidget {
           IconButton(
             onPressed: () {},
             // ignore: deprecated_member_use
-            icon: SvgPicture.asset('assets/images/more-apps.svg',color: primaryColor,),
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              color: primaryColor,
+            ),
           ),
           const SizedBox(
             width: 10,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical:10.0).copyWith(right: 10),
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0).copyWith(right: 10),
             child: MaterialButton(
               color: const Color(0xff1A73EB),
               onPressed: () {},
@@ -55,6 +61,24 @@ class WebScreenLayout extends StatelessWidget {
             ),
           )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.25),
+            const Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Search(),
+                  ],
+                ),
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
